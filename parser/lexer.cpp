b8a7812a5f0d64c5c9704e4b9f73563d73ad41d0
Ian Finlayson <finlayson@umw.edu>
2014-05-10 22:33:23 -0400

    Worked on hand written lexer a bit.
    It now works on a simple test case - without $ at the end :)
    Needs more testing, however...

--- parser/lexer.cpp
+++ parser/lexer.cpp
@@ -6,15 +6,21 @@
 #include "parser.gen.hpp"
 
 using std::cin;
-
+using namespace std;
 
 /* whether or not we have seen whitespace so far this line */
 int start_of_line = 1;
 
+/* the number of spaces used for one indentation level */
+int spaces_per_indent = 0;
 
+/* the current number of indent levels */
+int indent_level = 0;
 
+/* the number of DEDENTs to return before anything else */
+int dedents_left = 0;
 
-
+/* TODO this should be the one in tetra.cpp when done */
 void REPLfail(std::string m) {
   std::cout << m << std::endl;
   exit(1);
@@ -50,7 +56,8 @@ int lookupId(const std::string& id) {
 
 /* lex an identifier (or reserved word) given a start */
 int lexIdent(int start) {
-  std::string id((char) start, 1);
+  std::string id;
+  id.push_back((char) start);
 
   while (isalnum(cin.peek( )) || cin.peek( ) == '_') {
     id.push_back(cin.get( ));
@@ -115,12 +122,29 @@ int lexString( ) {
  * horrible hacks) to detect EOF and add DEDENT tokens - which makes it tough to do
  * whitespace based blocking a la Python */
 int yylex( ) {
+  /* if there are dedents left to return, do it */
+  if (dedents_left > 0) {
+    dedents_left--;
+    indent_level--;
+    return TOK_DEDENT;
+  }
+
   /* read in the next character in the stream */
   int next = cin.get( );
 
   /* check for EOF */
   if (cin.eof( )) {
-    return 0;
+    if (indent_level != 0) {
+      dedents_left = indent_level;
+      return yylex( );
+    } else {
+      return 0;
+    }
+  }
+
+  /* we do NOT allow tabs */
+  if (next == '\t') {
+    REPLfail("Tab characters are not allowed in Tetra!");
   }
 
   /* if it's a new line, set to beginning of line and return it */
@@ -130,21 +154,73 @@ int yylex( ) {
   }
 
   /* if it's whitespace, NOT at the start of a line, ignore it */
-  if (!start_of_line && (next == ' ' || next == '\t')) {
+  if (!start_of_line && next == ' ') {
     /* recurse to skip it */
     return yylex( );
   }
 
   /* if it's whitespace and IS at the start of the line */
-  if (start_of_line && (next == ' ' || next == '\t')) {
-    /* TODO */
+  if (start_of_line && next == ' ') {
+    /* count the spaces */
+    int spaces = 1;
+    while (cin.peek( ) == ' ') {
+      cin.get( );
+      spaces++;
+    }
+    start_of_line = 0;
 
+    //std::cout << "spaces = " << spaces << std::endl
+    //  << "indent_level = " << indent_level << std::endl;
+
+    /* if this is the first one, treat it as the level */
+    if (spaces_per_indent == 0) {
+      spaces_per_indent = spaces;
+    }
+
+    /* level is spaces / spaces_per_indent */
+    int level = spaces / spaces_per_indent;
+    if ((spaces % spaces_per_indent) != 0) {
+      REPLfail("Indentation level inconsistent.");
+    }
+
+    /* if the level is greater than the current one (by one) indent */
+    if (level == (indent_level + 1)) {
+      indent_level++;
+      return TOK_INDENT;
+    }
+
+    /* If the level is EVEN greater than that, error */
+    if (level > indent_level) {
+      REPLfail("Too much indentation.");
+    }
+
+    /* if the level is less than the current one */
+    if (level < indent_level) {
+      /* set dedents_left to the difference */
+      dedents_left = indent_level - level;
+
+      /* recurse */
+      return yylex( );
+    }
+
+    /* if it's the same, just move on */
+    if (level == indent_level) {
+      return yylex( );
+    }
+
+  /* else it's the start of line, but NOT whitespace */
   } else if (start_of_line) {
-    /* else it's the start of line, but NOT whitespace TODO */
-
+    start_of_line = 0;
+    /* if there is indentation, set the dedents to return to that - 1, and return 1 */
+    if (indent_level > 0) {
+      dedents_left = indent_level - 1;
+      indent_level = 0;
+      return TOK_DEDENT;
+    }
   }
 
-
+  /* it's not start of line any more */
+  start_of_line = 0;
 
   /* if it's a letter or underscore, we have an identifier (or reserved word) */
   if (isalpha(next)) {
@@ -165,6 +241,7 @@ int yylex( ) {
     /* get the new line */
     cin.get( );
     /* return the new line */
+    start_of_line = 1;
     return TOK_NEWLINE;
   }
 
