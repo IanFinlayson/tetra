3be0f91a96941c355871d69efd4d0f52d26d74dd
Ian Finlayson <finlaysoni@gmail.com>
2014-05-12 10:36:26 -0400

    More arranging.

--- parser/parse-test.cpp
+++ parser/parse-test.cpp
@@ -5,26 +5,6 @@
 #include "tetra.hpp"
 #include "parser.gen.hpp"
 
-extern int yylineno;
-int yylex( );
-
-/* node member functions */
-Node::Node(NodeType node_type) {
-  this->node_type = node_type;
-}
- 
-void Node::addChild(Node* child) {
-  children.push_back(child);
-}
-
-void Node::setDataType(DataType data_type) {
-  this->data_type = data_type;
-}
-
-void Node::setIdentifier(std::string identifier) {
-  this->identifier = identifier;
-}
-
 /* the main function */
 int main(int argc, char** argv) {
   /* if a file was passed, use that over stdin */
---
+++ parser/tetra.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include "tetra.hpp"
+#include "parser.gen.hpp"
+
+/* node member functions */
+Node::Node(NodeType node_type) {
+  this->node_type = node_type;
+}
+ 
+void Node::addChild(Node* child) {
+  children.push_back(child);
+}
+
+void Node::setDataType(DataType data_type) {
+  this->data_type = data_type;
+}
+
+void Node::setIdentifier(std::string identifier) {
+  this->identifier = identifier;
+}
+
+
