cb67c9a244a092881d8599d193d9b0922133b049
Ian Finlayson <finlaysoni@gmail.com>
2016-03-06 14:06:34 -0600

    work on site

--- site/syntax.js
+++ site/syntax.js
@@ -8,16 +8,16 @@ function applySyntax() {
     /* find all the <pre><code> blocks */
     var snippets = document.querySelectorAll('pre code');
 
-    snippets[0].style.backgroundColor = "red"; 
-
     /* highlight each one */
     for (i = 0; i < snippets.length; i++) {
+        /* get existing text */
         var text = snippets[i].textContent;
-        snippets[i].textContent = "Heyyo";
-        alert(text);
-        //highlighted = text.replace(/def/g, "heyoo");
-        //snippet.innerHTML = highlighted;
-        //alert(highlighted);
+        
+        /* apply the replacements */
+        highlighted = text.replace(/def/g, "heyoo");
+
+        /* write it back */
+        snippets[i].textContent = highlighted;
     }
 }
 
