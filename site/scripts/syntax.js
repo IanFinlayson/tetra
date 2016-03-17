5280a8e8db049cc819bf22e1b81605d409c89e66
Ian Finlayson <finlaysoni@gmail.com>
2016-03-17 15:09:05 -0500

    work on site

--- site/scripts/syntax.js
+++ site/scripts/syntax.js
@@ -2,7 +2,7 @@
  * apply simple syntax highlighting to Tetra code
  */
 
-keywords = ["class", "def", "if", "elif", "else", "while", "for", "in", "parallel", "return", "open", "import", "lambda", "background", "wait", "lock", "init", "global", "init", "self"];
+keywords = ["class", "def", "if", "elif", "else", "while", "for", "in", "parallel", "return", "open", "import", "lambda", "background", "wait", "lock", "init", "global", "init", "self", "constant"];
 types = ["int", "real", "string", "bool", "task", "mutex"];
 builtins = ["print", "len", "read_int", "read_string", "read_bool", "read_real", "array", "keys"];
 
