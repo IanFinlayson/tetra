d13c1109728119640d4a82f28bb9979f7d9e47ef
Jon <jonblauvelt1@gmail.com>
2016-07-11 12:48:05 -0400

    [interpreter] Add ttypes.h to repo.

---
+++ source/interpreter/tTypes.h
@@ -0,0 +1,57 @@
+/* This file defines basic tetra data types as
+ * wrappers around their c++ counterparts */
+
+
+
+class tbool {
+  public:
+    /* constructors */
+    tbool() {this->b = false;} 
+    tbool(const bool& b) {this->b = b;} 
+    tbool(const tbool& other) {this->b = other.b;}
+
+    /*assignment */
+    tbool operator=(const tbool& other) {
+      this->b = other.b;
+      return *this;
+    }
+
+    /* conversion */
+    operator bool const (){return this->b;}
+
+    /* logical operators */
+    tbool operator&&(const tbool& other) {return tbool(this->b && other.b);} 
+    tbool operator||(const tbool& other) {return tbool(this->b || other.b);} 
+    tbool operator!() {return tbool(!this->b);}
+
+    /* comparison operators */ 
+    tbool operator==(const tbool& other) {return tbool(this->b == other.b);}
+    tbool operator<(const tbool& other) {return tbool(this->b < other.b);}
+    tbool operator<=(const tbool& other) {return tbool(this->b <= other.b);}
+    tbool operator>(const tbool& other) {return tbool(this->b > other.b);}
+    tbool operator>=(const tbool& other) {return tbool(this->b >= other.b);}
+
+  private:
+    bool b;
+};
+
+class tint {
+  public:
+  private:
+};
+
+class treal {
+  public:
+  private:
+};
+/*
+class tstring {
+  public:
+  private:
+};
+
+*/
+class tnone {
+  public:
+  private:
+};
