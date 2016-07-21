bd890643b63e7b3ccff47302da579945757a762f
Ian Finlayson <finlaysoni@gmail.com>
2016-07-21 09:45:53 -0400

    [SITE] update some information

--- site/scripts/syntax.js
+++ site/scripts/syntax.js
@@ -3,7 +3,7 @@
  */
 
 /* lists of highlighted elements */
-keywords = ["class", "def", "if", "elif", "else", "while", "for", "in", "parallel", "return", "open", "import", "lambda", "background", "wait", "lock", "var", "init", "self", "constant", "none"];
+keywords = ["class", "def", "if", "elif", "else", "while", "for", "in", "parallel", "return", "open", "import", "lambda", "background", "wait", "lock", "var", "init", "self", "constant", "none", "global"];
 types = ["int", "real", "string", "bool", "task", "mutex"];
 builtins = ["print", "len", "read_int", "read_string", "read_bool", "read_real", "array", "keys"];
 
