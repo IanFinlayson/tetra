1f5bb4f86004080e4957a93f16962ec4c28ab663
Jon <jonblauvelt1@gmail.com>
2016-09-12 21:23:34 -0400

    [Type Checker] Remove debugging messages.

--- source/lib/types.cpp
+++ source/lib/types.cpp
@@ -378,10 +378,8 @@ DataType* inferRead(Node* functionCall) {
  * infers it */
 DataType* inferStdlib(Node* functionCall, Node* function, bool& is_stdlib) {
     is_stdlib = true;
-    std::cout << "numchildren = " + std::to_string(functionCall->getNumChildren()) + '\n';
-    std::cout << "stl function is : (" + functionCall->child(0)->getStringvalue() << ")"<< std::endl;
+
     if (functionCall->child(0)->getStringvalue() == "print") {
-        std::cout << "hellooooo\n";
         return inferPrint(functionCall, function);
     }
 
@@ -397,7 +395,6 @@ DataType* inferStdlib(Node* functionCall, Node* function, bool& is_stdlib) {
     }
 
     is_stdlib = false;
-    std::cout << "just set is_stdlib to false and is_stdlib = " + std::to_string(is_stdlib) + '\n';
     return NULL;
 }
 
@@ -909,9 +906,6 @@ DataType* inferExpressionPrime(Node* expr, Node* function) {
             bool is_stdlib;
             lhs = inferStdlib(expr, function, is_stdlib);
             if (is_stdlib) {
-                std::cout << "is_stdlib : " + std::to_string(is_stdlib) + '\n';
-                std::cout << "stl func is : " + expr->child(0)->getStringvalue();
-                std::cout << "\n";
                 return new DataType(*lhs);
             }
 
