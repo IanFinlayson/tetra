1fbe89761918e31aa8762a218a83f80a4051d66c
Ian Finlayson <finlaysoni@gmail.com>
2017-02-10 13:11:11 -0500

    Worked on test cases, all working ones have output, moved broken ones into another directory to work on one by one.

--- source/tests/run.sh
+++ source/tests/run.sh
@@ -7,7 +7,13 @@ fail=0
 for file in *.ttr
 do 
   ../../bin/tetra $file > ./output/tmp.txt
-  diffWords=`diff ./output/tmp.txt ./output/${file}.out | wc -w`
+  if [ ! -e "./output/${file}.out" ]
+  then
+      echo "~~~~~~~ ./output/${file}.out not found ~~~~~~~~~"
+      diffWords="100"
+  else
+      diffWords=`diff ./output/tmp.txt ./output/${file}.out | wc -w`
+  fi
   if [ $diffWords -ne 0 ]
   then
     fail=$(($fail + 1))
